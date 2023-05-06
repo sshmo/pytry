5c83742151713090532df83975b5d6ac21d47095
sshmo <nabaeazim1@gmail.com>
2023-05-06 06:15:01 +0330

    Added specifications

---
+++ pytry/p_1.py
@@ -0,0 +1,23 @@
+"""
+Write a program that:
+
+    reads 10 numbers from the input.
+    prints the number that has the highest number of divisors of the prime number \
+    along with the number of its prime divisors in the output.
+    If there are several numbers in this state, print the largest one.
+
+Input:
+123
+43
+54
+12
+76
+84
+98
+678
+543
+231
+
+Output:
+678 3
+"""
