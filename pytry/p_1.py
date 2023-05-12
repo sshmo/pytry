bdc5266982e12659ea42a14d2ddaee9a6bb8feb9
sshmo <nabaeazim1@gmail.com>
2023-05-12 08:50:26 +0330

    Added get_prime_list and has_prime_factor

--- pytry/p_1.py
+++ pytry/p_1.py
@@ -2,8 +2,8 @@
 Write a program that:
 
     reads 10 numbers from the input.
-    prints the number that has the highest number of divisors of the prime number \
-    along with the number of its prime divisors in the output.
+    prints the number that has the highest number of prime factors \
+    along with the number of its prime factors in the output.
     If there are several numbers in this state, print the largest one.
 
 Input:
@@ -22,6 +22,9 @@ Output:
 678 3
 """
 
+from math import floor
+from typing import List
+
 
 def get_nums(input_func):
     """reads 10 numbers from the input and returns list of them"""
@@ -35,10 +38,32 @@ def get_nums(input_func):
     return nums
 
 
+def has_prime_factor(number: int, primes: list):
+    """given prime list, checks if number has prime factors"""
+
+    for prime in primes:
+        if number > prime and number % prime == 0:
+            return True
+    return False
+
+
+def get_prime_list(nums: List):
+    "given numbers list, returns max prime factors list"
+
+    max_num = floor(max(nums) ** 0.5) + 1
+    primes = []
+    for i in range(2, max_num):
+        if not has_prime_factor(i, primes):
+            primes.append(i)
+    return primes
+
+
 def main():  # pragma: no cover
     """reads 10 numbers from the input and prints the numbers"""
 
     numbers = get_nums(input)
+    prime_list = get_prime_list(numbers)
+    print(prime_list)
     print(numbers)
 
 
--- tests/test_p_1.py
+++ tests/test_p_1.py
@@ -1,7 +1,7 @@
 import random
 from string import ascii_letters
 
-from pytry.p_1 import get_nums
+from pytry.p_1 import get_nums, get_prime_list, has_prime_factor
 
 
 def mock_input():
@@ -13,3 +13,18 @@ def test_get_nums():
     assert len(nums) == 10
     for num in nums:
         assert isinstance(num, int)
+
+
+def test_has_prime_factor():
+    assert not (has_prime_factor(2, [2]))
+    assert has_prime_factor(4, [2])
+
+
+def test_get_prime_list():
+    primes = [2, 3, 5]
+    prime_list = get_prime_list([1, 2, 48])
+    assert len(prime_list) == len(primes)
+    for num in prime_list:
+        assert num in primes
+    for num in primes:
+        assert num in prime_list
