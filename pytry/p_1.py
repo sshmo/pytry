e50908db3f2d2c9b57dc46fe005bf89625f48162
sshmo <nabaeazim1@gmail.com>
2023-05-12 19:59:32 +0330

    Added get_prime_count, updated main and tests

--- pytry/p_1.py
+++ pytry/p_1.py
@@ -22,12 +22,15 @@ Output:
 678 3
 """
 
-from math import floor
-from typing import List
+from typing import Any, Dict, List
 
 
-def get_nums(input_func):
-    """reads 10 numbers from the input and returns list of them"""
+def get_nums(input_func: Any) -> List[int]:
+    """
+    reads 10 numbers from the input and returns list of them
+    :param input_func: a function for generwtiong input strings
+    :return  a list of integers
+    """
     nums = []
     while True:
         num = input_func()
@@ -38,19 +41,26 @@ def get_nums(input_func):
     return nums
 
 
-def has_prime_factor(number: int, primes: list):
-    """given prime list, checks if number has prime factors"""
-
+def has_prime_factor(number: int, primes: List[int]) -> bool:
+    """
+    given prime list, checks if number has prime factors
+    :param number: an integer to check for prime factor
+    :param primes: list of prime factors to chack
+    :return: bool: return True or False
+    """
     for prime in primes:
         if number > prime and number % prime == 0:
             return True
     return False
 
 
-def get_prime_list(nums: List):
-    "given numbers list, returns max prime factors list"
-
-    max_num = floor(max(nums) ** 0.5) + 1
+def get_prime_list(nums: List[int]) -> List[int]:
+    """
+    given numbers list, returns all prime numbers lower than max number
+    :param nums: list of prime factors tp chack
+    :return: all prime numbers lower than max number
+    """
+    max_num = max(nums) + 1
     primes = []
     for i in range(2, max_num):
         if not has_prime_factor(i, primes):
@@ -58,14 +68,29 @@ def get_prime_list(nums: List):
     return primes
 
 
-def main():  # pragma: no cover
-    """reads 10 numbers from the input and prints the numbers"""
+def get_prime_count(nums: List, prime_list) -> Dict[int, int]:
+    "given numbers list, returns count of prime factors for each number"
 
-    numbers = get_nums(input)
+    factor_num = {}
+    for number in nums:
+        factor_num[number] = 0
+        for prime in prime_list:
+            if number % prime == 0:
+                factor_num[number] += 1
+    return factor_num
+
+
+def main(input_func: Any) -> str:
+    """reads 10 numbers from the input and prints max count of prime factors for max number"""
+
+    numbers = get_nums(input_func)
     prime_list = get_prime_list(numbers)
-    print(prime_list)
-    print(numbers)
+    factor_count = get_prime_count(numbers, prime_list)
+    print(factor_count)
+    max_value = max(factor_count.values())
+    keys = [key for key, value in factor_count.items() if value == max_value]
+    return f"{max(keys)} {max_value}"
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    print(main(input))
--- tests/test_p_1.py
+++ tests/test_p_1.py
@@ -1,7 +1,7 @@
 import random
 from string import ascii_letters
 
-from pytry.p_1 import get_nums, get_prime_list, has_prime_factor
+from pytry.p_1 import get_nums, get_prime_count, get_prime_list, has_prime_factor, main
 
 
 def mock_input():
@@ -21,10 +21,33 @@ def test_has_prime_factor():
 
 
 def test_get_prime_list():
-    primes = [2, 3, 5]
-    prime_list = get_prime_list([1, 2, 48])
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+    prime_list = get_prime_list([1, 2, 32])
     assert len(prime_list) == len(primes)
+    assert 1 not in prime_list
     for num in prime_list:
         assert num in primes
     for num in primes:
         assert num in prime_list
+
+
+def test_get_prime_count():
+
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    prime_list = [2, 3, 5, 7]
+    factor_num_dict = {1: 0, 2: 1, 3: 1, 4: 1, 5: 1, 6: 2, 7: 1, 8: 1, 9: 1, 10: 2}
+
+    factor_num = get_prime_count(nums, prime_list)
+    assert len(factor_num) == len(factor_num_dict)
+    for key, value in factor_num.items():
+        assert factor_num_dict[key] == value
+    for key, value in factor_num_dict.items():
+        assert factor_num[key] == value
+
+    assert factor_num
+
+
+def test_main():
+
+    data = [123, 43, 54, 12, 76, 84, 98, 678, 543, 231]
+    assert main(lambda: str(data.pop())) == "678 3"
