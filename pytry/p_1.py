1419c6f6139e319a3acdb26de5e15cf5c3d13fd2
sshmo <nabaeazim1@gmail.com>
2023-05-16 18:43:13 +0330

    Refactored max_prime

--- pytry/p_1.py
+++ pytry/p_1.py
@@ -27,94 +27,102 @@ Output:
 from typing import Any, Dict, List
 
 
-def get_nums(input_func: Any) -> List[int]:
-    """
-    Given random number of input strings; returns list of 10 integer numbers.
+class MaxPrime:
+    """MaxPrime."""
 
-    Args:
-        input_func: a function for generating input numbers.
+    @staticmethod
+    def get_nums(input_func: Any) -> List[int]:
+        """
+        Given random number of input strings; returns list of 10 integer numbers.
 
-    Returns:
-        a list of integers.
-    """
-    nums = []
-    while True:
-        num: str = input_func()
-        number = int(num) if num.isdigit() else None
-        _ = nums.append(number) if number else print("Not a number!")
-        if len(nums) == 10:
-            break
-    return nums
+        Args:
+            input_func: a function for generating input numbers.
+
+        Returns:
+            a list of integers.
+        """
+        nums = []
+        while True:
+            num = input_func()
+            num = int(num) if num.isdigit() else None
+            _ = nums.append(num) if num else print("Not a number!")
+            if len(nums) == 10:
+                break
+        return nums
+
+    @staticmethod
+    def has_prime_factor(number: int, primes: List[int]) -> bool:
+        """
+        Given prime list, checks if number has prime factors.
+
+        Args:
+            number: an integer to check for prime factor.
+            primes: list of prime factors to check.
+
+        Returns:
+            is prime or not.
+        """
+        for prime in primes:
+            if number > prime and number % prime == 0:
+                return True
+        return False
+
+    def get_prime_list(self, nums: List[int]) -> List[int]:
+        """
+        Given numbers list, returns all prime numbers lower than max number.
+
+        Args:
+            nums: all input numbers.
+
+        Returns:
+            all prime numbers lower than max number.
+        """
+        max_num = max(nums) + 1
+        primes = []
+        for i in range(2, max_num):
+            if not self.has_prime_factor(i, primes):
+                primes.append(i)
+        return primes
+
+    @staticmethod
+    def get_prime_count(nums: List, prime_list) -> Dict[int, int]:
+        """
+        Given numbers list, returns count of prime factors for each number.
+
+        Args:
+            nums: all input numbers.
+            prime_list: all prime numbers lower than max number.
+
+        Returns:
+            count of prime factors for each number.
+        """
+        factor_num = {}
+        for number in nums:
+            factor_num[number] = 0
+            for prime in prime_list:
+                if number % prime == 0:
+                    factor_num[number] += 1
+        return factor_num
+
+    def main(self, input_func: Any) -> str:
+        """
+        Given 10 numbers from the input; prints max count of prime factors for the max number.
+
+        Args:
+            input_func: a function for generating input numbers.
+
+        Returns:
+            max count of prime factors for the max number.
+        """
+        numbers = self.get_nums(input_func)
+        prime_list = self.get_prime_list(numbers)
+        factor_count = self.get_prime_count(numbers, prime_list)
+        max_value = max(factor_count.values())
+        keys = [key for key, value in factor_count.items() if value == max_value]
+        return f"{max(keys)} {max_value}"
 
 
-def is_prime(num: int):
-    """
-    Check if number is prime.
-
-    Args:
-        num: an integer.
-
-    Returns:
-        true if prime else false.
-    """
-    return all(num % x != 0 for x in range(2, int(num**0.5) + 1)) if num > 1 else False
-
-
-def get_prime_list(nums: List[int]) -> List[int]:
-    """
-    Given numbers list, returns all prime numbers lower than max number.
-
-    Args:
-        nums: all input numbers.
-
-    Returns:
-        all prime numbers lower than max number.
-    """
-    max_num = max(nums) + 1
-    primes = []
-    for i in range(2, max_num):
-        if is_prime(i):
-            primes.append(i)
-    return primes
-
-
-def get_prime_count(nums: List, prime_list) -> Dict[int, int]:
-    """
-    Given numbers list, returns count of prime factors for each number.
-
-    Args:
-        nums: all input numbers.
-        prime_list: all prime numbers lower than max number.
-
-    Returns:
-        count of prime factors for each number.
-    """
-    factor_num = {}
-    for number in nums:
-        factor_num[number] = 0
-        for prime in prime_list:
-            if number % prime == 0:
-                factor_num[number] += 1
-    return factor_num
-
-
-def main(input_func: Any) -> str:
-    """
-    Given 10 numbers from the input; prints max count of prime factors for the max number.
-
-    Args:
-        input_func: a function for generating input numbers.
-
-    Returns:
-        max count of prime factors for the max number.
-    """
-    numbers = get_nums(input_func)
-    prime_list = get_prime_list(numbers)
-    factor_count = get_prime_count(numbers, prime_list)
-    max_value = max(factor_count.values())
-    keys = [key for key, value in factor_count.items() if value == max_value]
-    return f"{max(keys)} {max_value}"
-
+max_prime = MaxPrime()
 
 if __name__ == "__main__":  # pragma: no cover
-    print(main(input))
+    print(max_prime.main(input))
