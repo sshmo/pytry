c7dc0640d020bcae3fea02cde368d9b1c42aa10b
sshmo <nabaeazim1@gmail.com>
2023-05-16 17:05:29 +0330

    Updated project confs

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import pytest
+from pyinstrument import Profiler
+
+TESTS_ROOT = Path.cwd()
+
+@pytest.fixture(autouse=True)
+def auto_profile(request):
+    PROFILE_ROOT = (TESTS_ROOT / ".profiles")
+    # Turn profiling on
+    profiler = Profiler()
+    profiler.start()
+
+    yield  # Run test
+
+    profiler.stop()
+    PROFILE_ROOT.mkdir(exist_ok=True)
+    results_file = PROFILE_ROOT / f"{request.node.name}.html"
+    with open(results_file, "w", encoding="utf-8") as f_html:
+        f_html.write(profiler.output_html())
